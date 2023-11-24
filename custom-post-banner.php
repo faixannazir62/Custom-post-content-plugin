803db65cd06516af4315dd8316e75d385a0b1ba5
faixannazir62 <faizanbhat46@gmail.com>
2023-11-24 23:49:36 +0530

    Plugin headers added

---
+++ custom-post-banner.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Plugin Name:     Custom post banner
+ * Plugin URI:      https://www.postcustombanner.com
+ * Description:     Create a custom banner for single post
+ * Version:         1.0.0
+ * Author:          Faizan Nazir
+ * Author URI:      https://www.faizannazir.com
+ * Text Domain:     cp-banner-fn
+ * Domain Path:     /languages
+ */
+
+
