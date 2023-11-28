5a90f7d3f41b14efc9f486d7d119b052fb540e48
faixannazir62 <faizanbhat46@gmail.com>
2023-11-29 01:02:47 +0530

    Function name invalid bug fixed

--- includes/save-custom-fields-input.php
+++ includes/save-custom-fields-input.php
@@ -27,24 +27,33 @@ function cpfn_save_custom_fields_input(){
      * 'cpfn_textarea_saved_css' and the selected post ID from the dropdown list with the meta key 
      * 'cpfn_selected_post_id'. The values are sanitized using the appropriate sanitization functions before saving them. 
      */ 
+     
+    if ( isset( $_POST['cpfn_textarea_css_input'] )){
 
-    // Update custom post css
-    update_post_meta( $post->ID, 'cpfn_textarea_saved_css', sanitize_textarea_field( $_POST['cpfn_textarea_css_input'] ) );
+            // Update custom post css
+            update_post_meta( $post->ID, 'cpfn_textarea_saved_css1', sanitize_textarea_field( $_POST['cpfn_textarea_css_input'] ) );
 
-    // Update selected post id
-    update_post_meta( $post->ID, 'cpfn_selected_post_id', sanitize_text_field( $_POST['cpfn_selected_post_id'] ));
+    }
+    if ( isset( $_POST['cpfn_selected_post_id'] )){
+
+            // Update selected post id
+            update_post_meta( $post->ID, 'cpfn_selected_post_id2', sanitize_text_field( $_POST['cpfn_selected_post_id'] ));
+   
+    }
+
+    
 
     // Excute the code if  'cpfn_selected_post_id' isset
     if ( ! empty( $_POST['cpfn_selected_post_id'] ) ){
 
         // unhook this function so it doesn't loop infinitely
-        remove_action( 'save_post', 'cpfn_save_selected_post' );
+        remove_action( 'save_post', 'cpfn_save_custom_fields_input' );
         
         // Set custom post content to selected post
         cpfn_set_custom_post_content( $post->ID, $_POST['cpfn_selected_post_id'] );
 
         // re-hook this function
-		add_action('save_post', 'cpfn_save_selected_post');
+		add_action('save_post', 'cpfn_save_custom_fields_input');
 
     }
 
