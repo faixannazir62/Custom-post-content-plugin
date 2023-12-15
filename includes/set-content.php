7daacc48633d8395e603980d067e67296c35506e
faixannazir62 <faizanbhat46@gmail.com>
2023-12-16 00:51:42 +0530

    Variable name renamed

--- includes/set-content.php
+++ includes/set-content.php
@@ -8,13 +8,13 @@ function cpfn_set_custom_post_content( $post_id, $selected_post_id ){
         $cpfn_selected_post = get_post( $selected_post_id );
 
         // Get content of custom post content
-        $cpfn_custom_post_content = get_post( $post_id );
+        $cpfn_custom_post_id = get_post( $post_id );
 
         // Check if both posts exist
-        if ( $cpfn_selected_post &&  $cpfn_custom_post_content ){
+        if ( $cpfn_selected_post &&  $cpfn_custom_post_id ){
 
             // Add Html tags
-            $cpfn_withtag_cp_content = '<div class="cpfn_content">' . $cpfn_custom_post_content->post_content . '</div>';
+            $cpfn_withtag_cp_content = '<div class="cpfn_content">' . $cpfn_custom_post_id->post_content . '</div>';
 
             // Condition is checking if the custom post content is already present in the selected post's content. 
             if ( strpos( $cpfn_selected_post->post_content, $cpfn_withtag_cp_content ) === false ){
