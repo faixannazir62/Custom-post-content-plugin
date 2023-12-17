ef3faab8a100dae1b9637d86ce08efb1bc5e55a6
faixannazir62 <faizanbhat46@gmail.com>
2023-12-18 00:09:50 +0530

    New file php file included in main php file

--- custom-post-content.php
+++ custom-post-content.php
@@ -22,4 +22,5 @@ include_once( plugin_dir_path( __FILE__ ) . 'includes/post-list-meta-box.php' );
 include_once( plugin_dir_path( __FILE__ ) . 'includes/save-custom-fields-input.php' );
 include_once( plugin_dir_path( __FILE__ ) . 'includes/set-content.php' );
 include_once( plugin_dir_path( __FILE__ ) . 'includes/set-meta-field-css.php' );
+include_once( plugin_dir_path( __FILE__ ) . 'includes/manage_columns.php' );
 
