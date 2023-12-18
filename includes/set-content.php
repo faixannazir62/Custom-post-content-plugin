24be9543f913a40cdba3e2e2a88bba4071bd294a
faixannazir62 <faizanbhat46@gmail.com>
2023-12-19 00:18:56 +0530

    Escape function added in class attribute

--- includes/set-content.php
+++ includes/set-content.php
@@ -16,7 +16,8 @@ function cpfn_set_custom_post_content( $post_id, $selected_post_id ){
              update_post_meta( $cpfn_selected_post_id->ID, 'cpfn_content_post_id', $cpfn_custom_post_id->ID );
 
             // Add Html tags
-            $cpfn_withtag_cp_content = '<div class="cpfn_content_'. $cpfn_selected_post_id->ID . '">' . $cpfn_custom_post_id->post_content . '</div>';
+            $cpfn_withtag_cp_content = '<div class="cpfn_content_'. esc_attr( $cpfn_selected_post_id->ID ) . '">
+            ' . $cpfn_custom_post_id->post_content . '</div>';
 
             // Condition is checking if the custom post content is already present in the selected post's content. 
             if ( strpos( $cpfn_selected_post_id->post_content, $cpfn_withtag_cp_content ) === false ){
