579a08f09881ae1f54c71fba85d7bf54d06e2948
faixannazir62 <faizanbhat46@gmail.com>
2023-12-18 22:12:39 +0530

    Display shortcodes on admin column

--- includes/manage_columns.php
+++ includes/manage_columns.php
@@ -31,3 +31,29 @@ function cpfn_add_shortcode_column( $columns ) {
 
 }
 add_filter('manage_cpfn_content_posts_columns', 'cpfn_add_shortcode_column');
+
+/**
+ * The function `cpfn_display_shortcode` is used to display a shortcode for a custom post type in the
+ * admin column.
+ * 
+ * @param column The  parameter is the name of the column in the post list table where the
+ * shortcode will be displayed. In this case, it is 'shortcode'.
+ * @param post_id The post ID is the unique identifier for a specific post in WordPress. It is used to
+ * retrieve and manipulate data associated with that post. In this case, the post ID is passed as a
+ * parameter to the `cpfn_display_shortcode` function to get the post title and generate a shortcode
+ * based on it
+ */
+function cpfn_display_shortcode( $column, $post_id){
+
+    if ( $column == 'shortcode' ){
+
+         $cpfn_post_title = get_the_title($post_id);
+
+         $cpfn_shortcode = '[cpfn title="' . esc_attr($cpfn_post_title) . '"]';
+
+          echo '<input type="text" readonly="readonly" value="' . esc_attr( $cpfn_shortcode) . 
+          '" class="cpfn-shortcode-input" onclick="this.select();" />';
+    }
+
+}
+add_action( 'manage_cpfn_content_posts_custom_column', 'cpfn_display_shortcode', 10, 2);
