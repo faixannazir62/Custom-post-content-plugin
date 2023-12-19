3355e33761100b3e1f3bf58bd555a759f796b409
faixannazir62 <faizanbhat46@gmail.com>
2023-12-19 14:32:31 +0530

    Apply shortcode content css code added and  few comments edited.

--- includes/set-meta-field-css.php
+++ includes/set-meta-field-css.php
@@ -1,15 +1,18 @@
 <?php
+
 /**
  * 
- * The function `cpfn_apply_custom_css` adds custom CSS to the head section of a WordPress website
- * based on certain conditions.
+ * The function `cpfn_apply_custom_css` applies custom CSS to specific pages based on the post type and
+ * post meta data.
  * 
  */
-function cpfn_apply_custom_css() {
 
+function cpfn_apply_custom_css() {
+    
+    // Get the current post ID
     $cpfn_post_id = get_the_ID();
      
-    // Add css to single page of 'cpfn_content' custom post 
+    // apply css to single page of 'cpfn_content' custom post 
     if( is_singular( 'cpfn_content' ) ) {
 
         // Get custom css
@@ -21,23 +24,31 @@ function cpfn_apply_custom_css() {
 
         }
     }
+
     // If it is other than this 'cpfn_content' custom post, then add custom css to the single page
     if( ! is_singular( 'cpfn_content' ) ) {
         
-        // Get custom css
+        // Get content post id
         $cpfn_post_content_id = get_post_meta( $cpfn_post_id, 'cpfn_content_post_id', true);
 
-         // Get custom css
+         // Get custom css by the content post id
         $cpfn_custom_css = get_post_meta( $cpfn_post_content_id, 'cpfn_textarea_saved_css', true);
 
-        // Get select post id
+        // Get select post id by the content post id
         $cpfn_selected_post_id = get_post_meta( $cpfn_post_content_id, 'cpfn_selected_post_id', true);
 
 
-        // If the content is added to the other post add css to that post
+        // Apply css to selected post by id's
         if( ! empty( $cpfn_custom_css ) && ! empty( $cpfn_selected_post_id ) ) {
 
             echo '<style id="cpfn-css">' . wp_strip_all_tags( $cpfn_custom_css ) . '</style>';
+
+        }
+
+        // Apply css to shortcode content posts
+        if ( ! empty( $cpfn_custom_css ) ){
+
+             echo '<style id="cpfn-css">' . wp_strip_all_tags( $cpfn_custom_css ) . '</style>';
 
         }
     }
