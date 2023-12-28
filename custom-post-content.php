5547339f2bfb1c4d607363fad7caffefed5cf3ba
faixannazir62 <faizanbhat46@gmail.com>
2023-12-28 22:00:31 +0530

    Enqueue style function added

--- custom-post-content.php
+++ custom-post-content.php
@@ -24,3 +24,24 @@ include_once( plugin_dir_path( __FILE__ ) . 'includes/set-content.php' );
 include_once( plugin_dir_path( __FILE__ ) . 'includes/set-meta-field-css.php' );
 include_once( plugin_dir_path( __FILE__ ) . 'includes/manage_columns.php' );
 include_once( plugin_dir_path( __FILE__ ) . 'includes/generate_post_shortcode.php' );
+
+
+/**
+ * 
+ * The function `cpfn_enqueue_scripts` enqueues a CSS file if the current post type is equal to
+ * 'cpfn_content' in the WordPress admin area.
+ * 
+ */
+function cpfn_enqueue_scripts(){
+
+          global $post_type;
+
+          // Checkif the current post type is equal to 'cpfn_content'. If it is, then  enqueue a CSS file
+          if( 'cpfn_content' === $post_type ){
+                    // Enqueue CSS file
+                    wp_enqueue_style( 'cpfn-meta-box-styles', plugin_dir_url( __FILE__ ) . 'admin/css/admin-styles-cpfn.css' );
+          }
+          
+
+}
+add_action( 'admin_enqueue_scripts', 'cpfn_enqueue_scripts');
